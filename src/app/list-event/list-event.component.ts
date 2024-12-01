74a6f18c1f870852e75a73919a486d830e53a4d9
ellexart <axelle.mazuy@gmail.com>
2024-12-01 19:20:50 +0100

    feat: add events list display

--- src/app/list-event/list-event.component.ts
+++ src/app/list-event/list-event.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PopupService } from '../services/popup.service';
+
+import { PaginationBarComponent } from '../pagination-bar/pagination-bar.component';
+import { Event } from '../models/event.model';
+import { EventService } from '../services/event.service';
 
 @Component({
-  selector: 'app-list-event',
+  selector: 'app-list-artist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, PaginationBarComponent],
   templateUrl: './list-event.component.html',
   styleUrl: './list-event.component.scss'
 })
 export class ListEventComponent {
+  @Input() id: string | null = null
+  events : Event[] = []
+
+  pageNumber: number = 1
+  pageSize: number = 4
+  totalPages: number = 0
+  minPage = 1
+  
+  constructor(private eventService: EventService, private activatedRoute : ActivatedRoute, private popupService : PopupService) {}
+
+  async ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(async (params) => {
+      this.id = params.get('id');
+      this.loadChildren()
+    });
+  }
+
+  pageChange(pageNumber: number) {
+    this.pageNumber = pageNumber;
+    this.loadChildren();
+  }
+
+  protected async loadChildren() {
+    this.eventService.searchEvents(this.pageNumber, this.pageSize, [])
+    .subscribe(
+      (response) => {
+        this.events = response.content;
+        this.totalPages = response.totalPages;
+      });
+  }
 
 }
