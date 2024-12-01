06c398ce72f3580883a52d86b8d83fc9759f3213
Etienne Binginot <etienne.binginot@gmail.com>
2024-12-01 19:23:48 +0100

    feat: event detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArtistComponent } from './artist/artist.component';
+import { EventComponent } from './event/event.component';
 
 export const routes: Routes = [
     { path : '', component: HomeComponent },
-    { path : 'artists/:id', component: ArtistComponent }
+    { path : 'artists/:id', component: ArtistComponent },
+    { path : 'events/:id', component: EventComponent}
 ];
